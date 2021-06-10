f96ee3874101547991d8fc9929c175dd58155b1f
Huey Vo (BP) <hvo@brightplan.com>
2021-06-10 13:59:15 -0700

    logging to test push token

--- dev/ota.js
+++ dev/ota.js
@@ -1,4 +1,4 @@
 // DO NOT delete or change this constant
 // it is updated by the script when we export our code to our OTA server
 // note that this is epoch time in seconds (and not milliseconds) because date function on Mac OS does not have milliseconds
-export const OTA_PACKAGE_TIMESTAMP_IN_SECONDS = 1623357707
+export const OTA_PACKAGE_TIMESTAMP_IN_SECONDS = 1623358664
